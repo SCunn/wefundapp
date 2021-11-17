1a11acf773bcc18f5a5267194cf7409bb7976519
scunn <shanecunn@hotmail.com>
2021-11-17 14:36:58 +0000

    components folder structure created, changes to App.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Styles
+import { Wrapper, Content, Circles } from './Header.styles';
+
+const Header = () => (
+    <Wrapper>
+        <Content>
+            <Circles>
+                Hi
+            </Circles>
+        </Content>
+    </Wrapper>
+);
+
+export default Header;
